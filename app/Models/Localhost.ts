09083368b8b0565d0b4187fe19a614d8c88e5f5f
THULIO23 <THULIOVICTOR23@GMAIL.COM>
2023-09-22 08:17:15 -0300

    thirdcommit

--- app/Models/Localhost.ts
+++ app/Models/Localhost.ts
@@ -2,12 +2,9 @@ import { DateTime } from 'luxon'
 import Hash from '@ioc:Adonis/Core/Hash'
 import { column, beforeSave, BaseModel } from '@ioc:Adonis/Lucid/Orm'
 
-export default class User extends BaseModel {
+export default class Localhost extends BaseModel {
   @column({ isPrimary: true })
   public id: number
-
-  @column()
-  public name: string
 
   @column()
   public email: string
@@ -16,7 +13,7 @@ export default class User extends BaseModel {
   public password: string
 
   @column()
-  public rememberMeToken?: string
+  public rememberMeToken: string | null
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
@@ -25,9 +22,9 @@ export default class User extends BaseModel {
   public updatedAt: DateTime
 
   @beforeSave()
-  public static async hashPassword (user: User) {
-    if (user.$dirty.password) {
-      user.password = await Hash.make(user.password)
+  public static async hashPassword (localhost: Localhost) {
+    if (localhost.$dirty.password) {
+      localhost.password = await Hash.make(localhost.password)
     }
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -3,3 +3,6 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+
+Route.post("/register", "AuthController.register")
+Route.post("/login", "AuthController.login")
